f7b31e34ce28bda8ffee467f78c1e15d948804cc
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-12-15 17:01:12 +0100

    Fix tests
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- cypress/integration/project/source_browsing.spec.ts
+++ cypress/integration/project/source_browsing.spec.ts
@@ -7,9 +7,7 @@ context("project source browsing", () => {
 
     // TODO(sos): add fake peers again when we have a peer testnet
     commands.createProjectWithFixture();
-  });
 
-  beforeEach(() => {
     cy.visit("./public/index.html#/profile/projects");
     cy.contains("platinum").click();
   });
@@ -172,27 +170,15 @@ context("project source browsing", () => {
                 "This repository is a data source for the Upstream front-end tests and the radicle-surf unit tests."
               );
 
-            // Going to a different path and then switching back to the root path
-            // shows the README again.
+            // Clicking the "Files" tab leads back to the project root, which
+            // renders the README.md
             commands.pick("source-tree").within(() => {
               cy.contains(".i-am-well-hidden").click();
             });
             commands
               .pick("project-screen", "file-view")
-              .contains("platinum / .i-am-well-hidden");
-            commands
-              .pick("project-screen", "file-view")
-              .contains("platinum")
-              .click();
-            commands.pick("project-screen", "file-view").contains("README.md");
-
-            commands.pick("source-tree").within(() => {
-              cy.contains(".i-too-am-hidden").click();
-            });
-            commands
-              .pick("project-screen", "file-view")
-              .contains("platinum / .i-too-am-hidden");
-            commands.pick("project-screen", "file-view", "root-link").click();
+              .contains(".i-am-well-hidden");
+            commands.pick("project-screen", "Files").click();
             commands.pick("project-screen", "file-view").contains("README.md");
 
             // Switching between different revisions shows the correct README
@@ -334,8 +320,6 @@ context("project source browsing", () => {
           .get("p")
           .should("have.class", "active");
 
-        commands.pick("file-view", "file-header").contains("platinum").click();
-
         commands
           .pick("horizontal-menu", "Files")
           .get("p")
@@ -428,47 +412,45 @@ context("project source browsing", () => {
             commands.pick("expand-special").click();
 
             commands.pick("source-tree").contains("-dash-").click();
-            cy.contains("platinum / special / -dash-").should("exist");
+            cy.contains("special / -dash-").should("exist");
 
             commands.pick("source-tree").contains("...").click();
-            cy.contains("platinum / special / ...").should("exist");
+            cy.contains("special / ...").should("exist");
 
             commands.pick("source-tree").contains(":colon:").click();
-            cy.contains("platinum / special / :colon:").should("exist");
+            cy.contains("special / :colon:").should("exist");
 
             commands.pick("source-tree").contains(";semicolon;").click();
-            cy.contains("platinum / special / ;semicolon;").should("exist");
+            cy.contains("special / ;semicolon;").should("exist");
 
             commands.pick("source-tree").contains("@at@").click();
-            cy.contains("platinum / special / @at@").should("exist");
+            cy.contains("special / @at@").should("exist");
 
             commands.pick("source-tree").contains("_underscore_").click();
-            cy.contains("platinum / special / _underscore_").should("exist");
+            cy.contains("special / _underscore_").should("exist");
 
             commands.pick("source-tree").contains("c++").click();
-            cy.contains("platinum / special / c++").should("exist");
+            cy.contains("special / c++").should("exist");
 
             commands.pick("source-tree").contains("faux\\path").click();
-            cy.contains("platinum / special / faux\\path").should("exist");
+            cy.contains("special / faux\\path").should("exist");
 
             commands.pick("source-tree").contains("i need some space").click();
-            cy.contains("platinum / special / i need some space").should(
-              "exist"
-            );
+            cy.contains("special / i need some space").should("exist");
 
             commands
               .pick("source-tree")
               .contains("qs?param1=value?param2=value2#hash")
               .click();
-            cy.contains(
-              "platinum / special / qs?param1=value?param2=value2#hash"
-            ).should("exist");
+            cy.contains("special / qs?param1=value?param2=value2#hash").should(
+              "exist"
+            );
 
             commands.pick("source-tree").contains("~tilde~").click();
-            cy.contains("platinum / special / ~tilde~").should("exist");
+            cy.contains("special / ~tilde~").should("exist");
 
             commands.pick("source-tree").contains("👹👹👹").click();
-            cy.contains("platinum / special / 👹👹👹").should("exist");
+            cy.contains("special / 👹👹👹").should("exist");
           });
         });
       });
